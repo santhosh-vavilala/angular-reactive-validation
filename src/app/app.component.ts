409b9a0bf79cfb9663ba75063045168089c0f944
Kumar Vavilala <s.kumar.vavilala@accenture.com>
2019-06-18 10:27:41 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-reactive';
+
+  myForm: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.myForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      height: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      leftEyePower: ['', Validators.required],
+      rightEyePower: ['', Validators.required],
+    });
+    this.updateValidators();
+  }
+
+  updateValidators(){
+    this.rightEyePowerForm.setValidators([Validators.required, Validators.min(this.leftEyePowerForm.value + 0.1)]);
+    this.rightEyePowerForm.updateValueAndValidity();
+  }
+
+  get leftEyePowerForm() {
+    return this.myForm.get('leftEyePower') as FormControl;
+  }
+
+  get rightEyePowerForm() {
+    return this.myForm.get('rightEyePower') as FormControl;
+  }
+
+  onSubmit(){
+    console.log(this.myForm.value);
+  }
 }
